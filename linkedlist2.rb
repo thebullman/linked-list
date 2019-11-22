0797f8afedbd93d3f31ad354e10db1cccac866c1
B. Kyrk Ullman <git@bkullman.com>
2019-11-22 23:55:51 +0000

    linkedlist2

---
+++ linkedlist2.rb
@@ -0,0 +1,37 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end 
+end
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+def reverse_list(list, previous=nil)
+  storedNode = list.next_node
+  list.next_node = previous
+  if storedNode
+    reverse_list(storedNode, list)
+  else
+    return list
+  end
+end
+
+print_values(node3)
+
+puts "-------"
+revlist = reverse_list(node3)
+print_values(revlist)
